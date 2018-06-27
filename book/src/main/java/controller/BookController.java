9bc7df972e9073c0649f791fcea8eaaa7a088204
iluohhhh <1621315833@qq.com>
2018-06-27 19:52:37 +0800

    Update BookController.java

--- book/src/main/java/controller/BookController.java
+++ book/src/main/java/controller/BookController.java
@@ -5,19 +5,4 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class BookController {
 
-//    @Autowired
-//    DuckService duckService;
-
-//    @RequestMapping(value="/index")
-//    public String guitar(){
-//        return "index";
-//    }
-
-//    @RequestMapping(value="/show")
-//    public String show(GuitarSpec guitarSpec,Model model){
-//        System.out.println(guitarSpec);
-//        List<Guitar> guitarList = guitarService.getGuitars(guitarSpec);
-//        model.addAttribute("guitarList",guitarList);
-//        return "show";
-//    }
 }
